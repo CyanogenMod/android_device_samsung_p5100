08064c556521ea26c0dd31f45161600a4702552c
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-06-19 13:10:24 +0200

    bootanimation

--- cm.mk
+++ cm.mk
@@ -18,7 +18,10 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GT-P5100
+PRODUCT_RELEASE_NAME := p5100
+
+# Boot animation
+TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
