fb0003e4396992f0be99ff5640df4660ac1e95b4
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-07 19:28:48 +0200

    p51xx: disable legacy sensors fusion
    Change-Id: I9862ea8b4bc655d7ea67548806ca5e8d8d967a38

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,9 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/p5100/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/p5100/bluetooth/vnd_espresso10.txt
 
+# Sensors
+BOARD_USE_LEGACY_SENSORS_FUSION := false
+
 # Security
 BOARD_USES_SECURE_SERVICES := true
 
