8b254e1710f22239c9a57521bab28c8033aecb42
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-26 02:15:57 +0200

    p5100: use opensource libril from omap4-common
    Change-Id: I4d0ef03dd4ae5c74fb96faf77c21859949f575d4

--- p5100.mk
+++ p5100.mk
@@ -31,7 +31,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
 
 # These are the hardware-specific features
