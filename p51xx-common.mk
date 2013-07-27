b12a9b83e1e7abac5e1099977b098c1ba918f8c5
codeworkx <codeworkx@cyanogenmod.org>
2013-07-28 00:15:59 +0200

    p51xx: unified fstab
    Change-Id: I77f3d3c5409303e4af60c76b36859e05c6658472

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -38,10 +38,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/ueventd.espresso10.rc:root/ueventd.espresso10.rc \
     $(LOCAL_PATH)/rootdir/fstab.espresso10:root/fstab.espresso10
 
-# Vold
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf
