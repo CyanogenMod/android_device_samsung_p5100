4e8597fafb779d51555bc2fffa08e2fbc3a774e3
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-11-11 16:15:22 -0500

    P5100: Don't reset inherited system-wide SELinux configs
    Change-Id: I12daa12b9287006f928d6d31f9b603720873443d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,10 +90,10 @@ BOARD_USE_LEGACY_SENSORS_FUSION := false
 BOARD_USES_SECURE_SERVICES := true
 
 # Selinux
-BOARD_SEPOLICY_DIRS := \
+BOARD_SEPOLICY_DIRS += \
     device/samsung/p5100/selinux
 
-BOARD_SEPOLICY_UNION := \
+BOARD_SEPOLICY_UNION += \
     file_contexts \
     file.te \
     device.te \
