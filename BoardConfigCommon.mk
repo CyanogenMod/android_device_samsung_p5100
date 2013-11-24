ffb45fe42dad1bbbd259a1a6f82ceda518fa94f0
Jiangyi <sam.andrew.jiang@gmail.com>
2013-11-24 20:14:00 +0100

    P51XX: Add BOARD_SUPPRESS_EMMC_WIPE flag
    While known to have chips affected by the Superbrick bug,
    no reports of bricks have actually occurred on this device.
    Better safe than sorry though.
    
    Change-Id: I37de80079e4d3adf60ad2e432900e24db7d2f730
    (cherry picked from commit b8a47d5eaccdbc685183077f0ff99865d3280d87)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,7 @@ BOARD_UMS_LUNFILE := "/sys/class/android_usb/f_mass_storage/lun0/file"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_EMMC_WIPE := true
 TARGET_RECOVERY_FSTAB := device/samsung/p5100/rootdir/fstab.espresso10
 RECOVERY_FSTAB_VERSION := 2
 
