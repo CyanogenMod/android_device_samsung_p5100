3eb7309829414d202f3c282727e14c846ff3a365
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-04-11 17:49:11 +0700

    Open source Samsung dock keyboard attach program
    Change-Id: I0c29b98f297b8cf8787db455d1ed703b89945592

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
     charger \
     charger_res_images
 
+# Samsung dock keyboard
+PRODUCT_PACKAGES += \
+    dock_kbd_attach
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
