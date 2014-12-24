bc272e912a75a10a763a4f8bfca54c4810e9894a
Andreas Blaesius <skate4life@gmx.de>
2014-12-24 12:17:28 +0100

    P51XX: add a working QWERTZ sec_keyboard layout
    Our existing keylayout wasn't working correctly in the past, so let's
    use a working QWERTZ layout for now.
    
    Thanks to Pevtrick @ www.android-hilfe.de
    
    Change-Id: I04114acf5c6cad4a36220756e04119d944bb70c2
    (cherry picked from commit 51c08163b6e69e56548331acd4f90e91146ccc5a)

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/usr/keychars/espresso-gpio-keypad.kcm:system/usr/keychars/espresso-gpio-keypad.kcm \
+    $(LOCAL_PATH)/usr/keychars/sec_keyboard.kcm:system/usr/keychars/sec_keyboard.kcm \
     $(LOCAL_PATH)/usr/keylayout/espresso-gpio-keypad.kl:system/usr/keylayout/espresso-gpio-keypad.kl \
     $(LOCAL_PATH)/usr/keylayout/sec_keyboard.kl:system/usr/keylayout/sec_keyboard.kl
 
