d7f9901d8937e1eb842bba2584a3d04cd530855a
Ziyan <jaraidaniel@gmail.com>
2015-05-08 02:47:02 +0200

    Use 44.1 kHz UI sounds
    Our fast paths sampling rate is 44.1 kHz, which
    conflicts with the default 48 kHz sounds.
    
    Change-Id: I1505b88180b5db6efb70ad6882c3b12a10989782

--- full_p5100.mk
+++ full_p5100.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Use 44.1 kHz UI sounds
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/p5100/p5100.mk)
