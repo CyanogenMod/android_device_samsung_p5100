78b30bc2d4e868d5764604eeb313c3c2eb659c29
Andreas Blaesius <skate4life@gmx.de>
2015-05-21 21:17:18 +0200

    P51XX: rework overlay
    Split CM and AOSP specific overlay
    
    Change-Id: I55e5b2fe359e202b1ec65bc77316629309b68fa4

--- p5100.mk
+++ p5100.mk
@@ -18,7 +18,7 @@ $(call inherit-product, device/samsung/p5100/p51xx-common.mk)
 
 LOCAL_PATH := device/samsung/p5100
 
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/aosp
 
 # Audio
 PRODUCT_COPY_FILES += \
