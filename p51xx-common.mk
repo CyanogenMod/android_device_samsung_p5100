c6da88e4fc27014fabbefb60af0a9acc164eb982
Andreas Blaesius <skate4life@gmx.de>
2015-07-16 21:53:04 +0200

    P51XX: cleanup / rework ramdisk parts for better readability (1/2)
    Change-Id: Ic5ea906ccbc348a4201628ac3269f5500e7830f7

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -31,12 +31,12 @@ PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 PRODUCT_LOCALES += mdpi
 
-# Init files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.espresso10.usb.rc:root/init.espresso10.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.espresso10.rc:root/init.espresso10.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.espresso10.rc:root/ueventd.espresso10.rc \
-    $(LOCAL_PATH)/rootdir/fstab.espresso10:root/fstab.espresso10
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.espresso10 \
+    init.espresso10.usb.rc \
+    init.espresso10.rc \
+    ueventd.espresso10.rc
 
 # GPS
 PRODUCT_COPY_FILES += \
