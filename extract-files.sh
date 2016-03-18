f0012ace7b4ae4ab17ea4b6cbcfb004b32f75be1
Andreas Blaesius <skate4life@gmx.de>
2016-03-18 12:56:07 +0100

    extract-files.sh: fix 2 typos
    Change-Id: I867405c7bbb5ba18e83ffd1b90bea7f7a4de2e38

--- extract-files.sh
+++ extract-files.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-./../../../device/samsung/espersso-common/extract-file-s3g.sh
+./../../../device/samsung/espresso-common/extract-files-3g.sh
