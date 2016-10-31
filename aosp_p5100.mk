07b4eaf6806290f96a92bff613bb9f68089d9f33
Andreas Blaesius <skate4life@gmx.de>
2016-10-31 23:48:37 +0100

    Some nitpicks
    Change-Id: I19be9c6026336229df8cdd1051f5b2c05e22c0d5

--- aosp_p5100.mk
+++ aosp_p5100.mk
@@ -14,14 +14,14 @@
 # limitations under the License.
 #
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit device specific configuration
 $(call inherit-product, device/samsung/p5100/device.mk)
 
-PRODUCT_DEVICE := p5100
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 PRODUCT_NAME := aosp_p5100
+PRODUCT_DEVICE := p5100
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := GT-P5100
 PRODUCT_MANUFACTURER := Samsung
